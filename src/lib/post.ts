eec39d97545f5f6b6690802339401a307c006920
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-26 18:39:54 +0530

    log

--- src/lib/post.ts
+++ src/lib/post.ts
@@ -62,7 +62,7 @@ const fetchPosts = async () => {
 export async function getAllPostsFrontMatter(): Promise<Array<FrontMatter>> {
 	console.log("CHECK HERE BBB");
 	// console.error(fetchPosts);
-	// console.log(fetchPosts);
+	console.log(fetchPosts);
 	const files = readdirSync(BLOG_POSTS_DIR);
 	//fetchPosts();
 
