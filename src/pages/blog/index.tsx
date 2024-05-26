ad1012d4d84c42a7cc8813b5bfab4c5ef9b208fc
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-26 16:46:39 +0530

    hope this works

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,6 +1,8 @@
 import { Blog } from '~/components';
 import { getAllPostsFrontMatter } from '~/lib/post';
 import { Layout } from '~/layouts';
+import Post from './Post';
+import './App.css';
 
 import type { GetStaticProps } from 'next';
 
@@ -60,20 +62,30 @@ export default function BlogPage({ serialisedFrontmatters }: BlogProps): JSX.Ele
 
 	if (frontmatters.length <= 0) return <Blog.Error routeBlog={false} />;
 
-	const latestPost = frontmatters.shift();
+	//const latestPost = frontmatters.shift();
 
 	return (
 		<Layout.Default seo={{ title: 'nuro ─ blog' }}>
-			<div className="mt-8 sm:mt-16 mb-20 mx-0 sm:mx-6 lg:mb-28 lg:mx-8">
-				<div className="relative max-w-6xl mx-auto">
-					<Blog.Latest frontmatter={latestPost} />
-					<div className="mt-4 lg:mt-12 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:max-w-none">
-						{frontmatters.map((frontmatter, i) => (
-							<Blog.Post key={i} frontmatter={frontmatter} index={i} />
-						))}
-					</div>
-				</div>
+			<div class="container">
+				{this.state.posts.map((post, index) => (
+					<a key={index} href={`https://thetechdeck.hashnode.dev/${post.slug}`} >
+						<Post post={post} />
+					</a>
+				))}
 			</div>
+
 		</Layout.Default>
 	);
 }
+
+
+// {/* <div className="mt-8 sm:mt-16 mb-20 mx-0 sm:mx-6 lg:mb-28 lg:mx-8">
+// 	<div className="relative max-w-6xl mx-auto">
+// 		<Blog.Latest frontmatter={latestPost} />
+// 		<div className="mt-4 lg:mt-12 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:max-w-none">
+// 			{frontmatters.map((frontmatter, i) => (
+// 				<Blog.Post key={i} frontmatter={frontmatter} index={i} />
+// 			))}
+// 		</div>
+// 	</div>
+// </div> */}
