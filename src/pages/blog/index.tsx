f659a3256d75e0d9380f8195c6a62c0fc300cc05
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-27 19:43:16 +0530

    once more

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -134,61 +134,97 @@ const blo = function Blog() {
 
 		setPosts(result.data.publication.posts.edges);
 	};
-
-
 	return (
-		// <Layout.Default seo={{ title: 'nuro ─ blog' }}>
-		// 	<div className="mt-8 sm:mt-16 mb-20 mx-0 sm:mx-6 lg:mb-28 lg:mx-8">
-		// 		<div className="relative max-w-6xl mx-auto">
-		// 			{/* <Blog.Latest frontmatter={latestPost} /> */}
-		// 			<div className="mt-4 lg:mt-12 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:max-w-none">
-		// 				{posts.map((frontmatter, i) => (
-		// 					<Blog.Post key={i} frontmatter={frontmatter} index={i} />
-		// 				))}
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// </Layout.Default>
+		<div className="flex flex-wrap -m-4 justify-center whitespace-break-spaces">
+			{posts.map((post, index) => {
+				const { slug, title, brief, coverImage } = post.node;
 
-
-		<>
-			<h1 className="text-center items-center justify-center top-36 tracking-[20px] text-gray-500 text-3xl lg:text-4xl font-bold">Blogs</h1>
-			<section className="text-gray-300 body-font">
-				<div className="container px-5 py-24 mx-auto">
-					<div className="flex flex-wrap -m-4 justify-center whitespace-break-spaces">
-						{posts.map((c, index) => {
-
-							const key = JSON.stringify(c.coverImage.url);
-
-							console.log("END THE LOG");
-							// const { url } = c.coverImage.url;
-							return (
-								<div className="p-4 md:w-1/3" key={index}>
-									<a href={`https://blog.developerdao.com//${c.slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
-										<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
-											<Image
-												className="lg:h-48 md:h-36 w-full object-cover object-center"
-												src={key || ''}
-												alt={c.slug || ''}
-												width={250}
-												height={250}
-											/>
-											<div className="p-6">
-												<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
-													{c.title || ''}</h1>
-												<p className="leading-relaxed text-gray-500 mb-3">{c.brief || ''}</p>
-											</div>
-										</div>
-									</a>
+				return (
+					<div className="p-4 md:w-1/3" key={index}>
+						<a
+							href={`https://blog.developerdao.com/${slug}`}
+							className="block"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
+								<Image
+									className="lg:h-48 md:h-36 w-full object-cover object-center"
+									src={coverImage.url}
+									alt={title}
+									width={250}
+									height={250}
+								/>
+								<div className="p-6">
+									<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
+										{title}
+									</h1>
+									<p className="leading-relaxed text-gray-500 mb-3">
+										{brief}
+									</p>
 								</div>
-							)
-						})}
+							</div>
+						</a>
 					</div>
-				</div>
-			</section>
+				);
+			})}
+		</div>
+	);
 
-		</>
-	)
+
+	// return (
+	// <Layout.Default seo={{ title: 'nuro ─ blog' }}>
+	// 	<div className="mt-8 sm:mt-16 mb-20 mx-0 sm:mx-6 lg:mb-28 lg:mx-8">
+	// 		<div className="relative max-w-6xl mx-auto">
+	// 			{/* <Blog.Latest frontmatter={latestPost} /> */}
+	// 			<div className="mt-4 lg:mt-12 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:max-w-none">
+	// 				{posts.map((frontmatter, i) => (
+	// 					<Blog.Post key={i} frontmatter={frontmatter} index={i} />
+	// 				))}
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// </Layout.Default>
+
+
+	// 	<>
+	// 		<h1 className="text-center items-center justify-center top-36 tracking-[20px] text-gray-500 text-3xl lg:text-4xl font-bold">Blogs</h1>
+	// 		<section className="text-gray-300 body-font">
+	// 			<div className="container px-5 py-24 mx-auto">
+	// 				<div className="flex flex-wrap -m-4 justify-center whitespace-break-spaces">
+	// 					{posts.map((c, index) => {
+
+	// 						const key = JSON.stringify(c?.coverImage?.url);
+
+	// 						console.log("END THE LOG");
+	// 						// const { url } = c.coverImage.url;
+	// 						return (
+	// 							<div className="p-4 md:w-1/3" key={index}>
+	// 								<a href={`https://blog.developerdao.com//${c.slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
+	// 									<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
+	// 										<Image
+	// 											className="lg:h-48 md:h-36 w-full object-cover object-center"
+	// 											src={key || ''}
+	// 											alt={c.slug || ''}
+	// 											width={250}
+	// 											height={250}
+	// 										/>
+	// 										<div className="p-6">
+	// 											<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
+	// 												{c.title || ''}</h1>
+	// 											<p className="leading-relaxed text-gray-500 mb-3">{c.brief || ''}</p>
+	// 										</div>
+	// 									</div>
+	// 								</a>
+	// 							</div>
+	// 						)
+	// 					})}
+	// 				</div>
+	// 			</div>
+	// 		</section>
+
+	// 	</>
+	// )
 };
 
 
