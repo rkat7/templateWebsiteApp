d473ffe711dc28dacbbd26f8aba7598b67dd86f6
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-28 09:38:41 +0530

    heheheh

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -19,75 +19,6 @@ interface BlogProps {
 }
 
 
-// const query = `
-// query Publication {
-// 	publication(host: "https://thetechdeck.hashnode.dev/") {
-// 	isTeam
-// 	title
-// 	posts(first: 50) {
-// 		edges {
-// 		node {
-// 			slug
-//         	coverImage
-// 			title
-// 			brief
-// 			url
-// 		}
-// 		}
-// 	}
-// 	}
-// }`
-// 	;
-
-// const fetchPosts = async () => {
-// 	const response = await fetch('https://gql.hashnode.com', {
-// 		method: 'POST',
-// 		headers: {
-// 			'Content-type': 'application/json',
-// 		},
-// 		body: JSON.stringify({ query }),
-// 	})
-// 	const ApiResponse = await response.json();
-// 	return ApiResponse;
-
-// };
-
-
-// export const getStaticProps: GetStaticProps<BlogProps> = async () => {
-// 	const frontmatters = await getAllPostsFrontMatter();
-
-// 	return {
-// 		props: {
-// 			serialisedFrontmatters: JSON.stringify(frontmatters),
-// 		},
-// 	};
-// };
-
-// export default function BlogPage({ serialisedFrontmatters }: BlogProps): JSX.Element {
-// 	const frontmatters = JSON.parse(serialisedFrontmatters) as Array<FrontMatter>;
-
-// 	if (frontmatters.length <= 0) return <Blog.Error routeBlog={false} />;
-
-// 	const latestPost = frontmatters.shift();
-
-// 	return (
-// 		<Layout.Default seo={{ title: 'nuro ─ blog' }}>
-// 			<div className="mt-8 sm:mt-16 mb-20 mx-0 sm:mx-6 lg:mb-28 lg:mx-8">
-// 				<div className="relative max-w-6xl mx-auto">
-// 					<Blog.Latest frontmatter={latestPost} />
-// 					<div className="mt-4 lg:mt-12 grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 sm:max-w-none">
-// 						{frontmatters.map((frontmatter, i) => (
-// 							<Blog.Post key={i} frontmatter={frontmatter} index={i} />
-// 						))}
-// 					</div>
-// 				</div>
-// 			</div>
-// 		</Layout.Default>
-// 	);
-//}
-
-
-
 
 const blo = function Blog() {
 	// Added schema of Api querry to get the data from hashnode.
@@ -196,6 +127,10 @@ const blo = function Blog() {
 						{posts?.map((post) => {
 							console.log("lets log posts");
 							console.log(posts);
+							console.log(posts.slug);
+							console.log(posts.coverImage.url);
+							console.log(posts.title);
+							console.log(posts.slug);
 
 
 							console.log("END THE LOG");
