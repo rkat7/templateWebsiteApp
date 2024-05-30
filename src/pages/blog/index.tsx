5211798319a79fe529fc0105ec3f89631ae89b46
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-30 19:14:39 +0530

    objects map and keys with logs since no render last time

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -56,13 +56,6 @@ const blo = function Blog() {
 			body: JSON.stringify({ query }),
 		});
 		const result = await response.json();
-		// result.publication.posts.edges.forEach(post => {
-		// 	post.node.imageUrl = post.coverImage.url;
-		// 	console.log("has it changed");
-		// 	console.log(post);
-		// });
-		console.log(result.toString);
-
 		setPosts(result.data.publication.posts.edges);
 	};
 	// return (
@@ -119,73 +112,53 @@ const blo = function Blog() {
 
 
 	return (
-		<>
-			<h1 className="text-center items-center justify-center top-36 tracking-[20px] text-gray-500 text-3xl lg:text-4xl font-bold">Blogs</h1>
+		<Layout.Default seo={{ title: 'nuro ─ blog' }} >
 			<section className="text-gray-300 body-font">
 				<div className="container px-5 py-24 mx-auto">
 					<div className="flex flex-wrap -m-4 justify-center whitespace-break-spaces">
 						{posts?.map((post) => {
 							console.log(post);
-							// console.log("lets log posts");
-							// console.log(posts);
-							// console.log(post[0]?.slug);
-							// console.log(post[0]?.coverImage?.url);
-							// console.log(post[0]?.title);
 
-
-							// console.log("plural works?");
-							// console.log(posts[0]?.slug);
-							// console.log(posts[0]?.coverImage?.url);
-							// console.log(posts[0]?.title);
-
-							// console.log("mixxxxxxxxxxxx");
-							// //console.log(posts[0]?.node?.slug);
-							// console.log(post[0]?.node?.slug);
-
-							//console.log(post?.node?.slug);
-
-
-							// console.log("END THE LOG");
 							console.log("watch out  below");
-							{
-								console.log("watch out here");
-								//post.map(i => console.log(i));
-								Object.keys(post).map((key, i) => {
-									console.log(post[key].slug);
-									console.log(post[key].coverImage);
-									console.log(post[key].title);
-									console.log(post[key].brief);
+
+							console.log("watch out here");
+
+							Object.keys(post).map((key, i) => {
+								// console.log(post[key].slug);
+								// console.log(post[key].coverImage);
+								// console.log(post[key].title);
+								// console.log(post[key].brief);
 
 
 
-									return (
-										<div className="p-4 md:w-1/3" key={post[key]?.slug}>
-											<a href={`https://blog.developerdao.com//${post[key].slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
-												<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
-													<Image
-														className="lg:h-48 md:h-36 w-full object-cover object-center"
-														src={post[key]?.coverImage?.url || ''}
-														alt={post[key]?.slug || ''}
-														width={250}
-														height={250}
-													/>
-													<div className="p-6">
-														<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
-															{post[key]?.title || ''}</h1>
-														<p className="leading-relaxed text-gray-500 mb-3">{post[key]?.brief || ''}</p>
-													</div>
+								return (
+									<div className="p-4 md:w-1/3" key={post[key]?.slug}>
+										<a href={`https://blog.developerdao.com//${post[key].slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
+											<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
+												<Image
+													className="lg:h-48 md:h-36 w-full object-cover object-center"
+													src={post[key]?.coverImage?.url || ''}
+													alt={post[key]?.slug || ''}
+													width={250}
+													height={250}
+												/>
+												<div className="p-6">
+													<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
+														{post[key]?.title || ''}</h1>
+													<p className="leading-relaxed text-gray-500 mb-3">{post[key]?.brief || ''}</p>
 												</div>
-											</a>
-										</div>
-									)
-								})
-							}
-						})}
+											</div>
+										</a>
+									</div>
+								)
+							})
+						}
+						)}
 					</div>
 				</div>
 			</section>
 
-		</>
+		</Layout.Default >
 	)
 };
 
