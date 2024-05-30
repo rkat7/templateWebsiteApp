5e0926c178f7337067269c2c1a87b597e8d279a3
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-30 19:33:55 +0530

    objects map and keys with logs since no render last time

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -135,13 +135,7 @@ const blo = function Blog() {
 									<div className="p-4 md:w-1/3" key={post[key]?.slug}>
 										<a href={`https://blog.developerdao.com//${post[key].slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
 											<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
-												<Image
-													className="lg:h-48 md:h-36 w-full object-cover object-center"
-													src={post[key]?.coverImage?.url || ''}
-													alt={post[key]?.slug || ''}
-													width={250}
-													height={250}
-												/>
+
 												<div className="p-6">
 													<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
 														{post[key]?.title || ''}</h1>
@@ -164,3 +158,12 @@ const blo = function Blog() {
 
 
 export default blo;
+
+
+{/* <Image
+className="lg:h-48 md:h-36 w-full object-cover object-center"
+src={post[key]?.coverImage?.url || ''}
+alt={post[key]?.slug || ''}
+width={250}
+height={250}
+/> */}
