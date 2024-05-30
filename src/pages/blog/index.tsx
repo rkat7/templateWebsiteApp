6dd1e56a645a92f0d8f5f28bf8e04173b309952e
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-30 20:10:07 +0530

    removed nested return

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -116,35 +116,30 @@ const blo = function Blog() {
 			<section className="text-gray-300 body-font">
 				<div className="container px-5 py-24 mx-auto">
 					<div className="flex flex-wrap -m-4 justify-center whitespace-break-spaces">
-						{posts?.map((post) => {
-							console.log(post);
-
-							console.log("watch out  below");
-
-							console.log("watch out here");
+						{posts?.map((post, kkk) => {
 
 							Object.keys(post).map((keyed, i) => {
 
-								return (
-									<div className="p-4 md:w-1/3" key={i}>
-										<a href={`https://blog.developerdao.com//${post[keyed].slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
-											<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
-												<Image
-													className="lg:h-48 md:h-36 w-full object-cover object-center"
-													src={post[keyed]?.coverImage?.url || ''}
-													alt={post[keyed]?.slug || ''}
-													width={250}
-													height={250}
-												/>
-												<div className="p-6">
-													<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
-														{post[keyed]?.title || ''}</h1>
-													<p className="leading-relaxed text-gray-500 mb-3">{post[keyed]?.brief || ''}</p>
-												</div>
+
+								<div className="p-4 md:w-1/3" key={i}>
+									<a href={`https://blog.developerdao.com//${post[keyed].slug || ''}`} className="block" target="_blank" rel="noopener noreferrer">
+										<div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden transform transition-all hover:scale-110 ">
+											<Image
+												className="lg:h-48 md:h-36 w-full object-cover object-center"
+												src={post[keyed]?.coverImage?.url || ''}
+												alt={post[keyed]?.slug || ''}
+												width={250}
+												height={250}
+											/>
+											<div className="p-6">
+												<h1 className="title-font text-lg font-medium text-gray-300 mb-3">
+													{post[keyed]?.title || ''}</h1>
+												<p className="leading-relaxed text-gray-500 mb-3">{post[keyed]?.brief || ''}</p>
 											</div>
-										</a>
-									</div>
-								)
+										</div>
+									</a>
+								</div>
+
 							})
 						}
 						)}
