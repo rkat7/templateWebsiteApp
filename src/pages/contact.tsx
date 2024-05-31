ece81c184f8629290acf33ba5d3272c50f703120
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-31 21:32:24 +0530

    updates

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,23 +4,24 @@ import { Layout } from '~/layouts';
 const contact: React.FC = () => {
     return (
         <Layout.Default seo={{ title: 'Get in touch' }} >
-            <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-6 sm:py-12">
-                <div className="bg-white p-6 rounded-lg shadow-lg max-w-md w-full text-center">
-                    <h1 className="text-2xl font-bold mb-6">Get in Touch</h1>
+
+            <div className="flex flex-col items-center justify-center min-h-screen py-6 sm:py-12">
+                <div className="bg-transparent p-6 rounded-lg shadow-lg max-w-md w-full text-center border border-gray-200">
+                    <h1 className="text-2xl font-bold mb-6 text-gray-800">Get in Touch</h1>
 
                     <div className="mb-4">
                         <p className="text-gray-700 mb-2">I respond quick to emails</p>
                         <a href="mailto:kskrohith@gmail.com">
-                            <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                            <button className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white px-4 py-2 rounded hover:from-blue-600 hover:to-indigo-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
                                 Email Me
                             </button>
                         </a>
                     </div>
 
                     <div>
-                        <p className="text-gray-700 mb-2">2 clicks away from a tête-à-tête</p>
+                        <p className="text-gray-700 mb-2">Two clicks away from a tête-à-tête</p>
                         <a href="https://calendly.com/kskrohith" target="_blank" rel="noopener noreferrer">
-                            <button className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300">
+                            <button className="bg-gradient-to-r from-green-500 to-teal-500 text-white px-4 py-2 rounded hover:from-green-600 hover:to-teal-600 focus:outline-none focus:ring-2 focus:ring-green-300">
                                 Schedule a Call
                             </button>
                         </a>
