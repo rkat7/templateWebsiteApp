8eb25840c83b6e43a1d3dc5bce57e3bdc680b2c1
SaiKrishnaRohith.Kattamuri <saikrishnarohith.kattamuri@target.com>
2024-05-31 22:19:36 +0530

    button update

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -4,23 +4,23 @@ import { Layout } from '~/layouts';
 const contact: React.FC = () => {
     return (
         <Layout.Default seo={{ title: 'Get in touch' }} >
-            <div className="relative flex flex-col items-center justify-center min-h-screen py-6 sm:py-12 bg-gray-100">
-                <div className="relative p-6 rounded-lg max-w-md w-full text-center z-10">
-                    <h1 className="text-2xl font-bold mb-6 text-gray-800">Get in Touch</h1>
+            <div className="relative flex flex-col items-center justify-center min-h-screen py-12 bg-gray-100">
+                <div className="relative p-8 rounded-lg max-w-md w-full text-center z-10 shadow-md">
+                    <h1 className="text-3xl font-bold mb-6 text-gray-800">Get in Touch</h1>
 
-                    <div className="mb-4">
-                        <p className="text-gray-700 mb-2">I respond quick to emails</p>
+                    <div className="mb-8">
+                        <p className="text-lg text-gray-700 mb-4">I respond quickly to emails</p>
                         <a href="mailto:kskrohith@gmail.com">
-                            <button className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white px-4 py-2 rounded-full hover:from-blue-600 hover:to-indigo-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+                            <button className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white text-lg px-6 py-3 rounded-full hover:from-blue-600 hover:to-indigo-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
                                 Email Me
                             </button>
                         </a>
                     </div>
 
                     <div>
-                        <p className="text-gray-700 mb-2">Two clicks away from a tête-à-tête</p>
+                        <p className="text-lg text-gray-700 mb-4">Two clicks away from a tête-à-tête</p>
                         <a href="https://calendly.com/kskrohith" target="_blank" rel="noopener noreferrer">
-                            <button className="bg-gradient-to-r from-green-500 to-teal-500 text-white px-4 py-2 rounded-full hover:from-green-600 hover:to-teal-600 focus:outline-none focus:ring-2 focus:ring-green-300">
+                            <button className="bg-gradient-to-r from-green-500 to-teal-500 text-white text-lg px-6 py-3 rounded-full hover:from-green-600 hover:to-teal-600 focus:outline-none focus:ring-2 focus:ring-green-300">
                                 Schedule a Call
                             </button>
                         </a>
